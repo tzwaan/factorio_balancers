b69d38f30a6e00353d4cc3ca26c599d5429fae67
Tijmen Zwaan <tijmen.zwaan@gmail.com>
2020-07-18 16:11:31 +0200

    Separate tests into different classes and files. Fix print of splitters.

---
+++ tests/test_blueprint.py
@@ -0,0 +1,42 @@
+import unittest
+
+
+from py_factorio_blueprints.exceptions import UnknownEntity
+from factorio_balancers.balancer import Balancer
+from factorio_balancers.exceptions import *
+
+
+class TestBlueprint(unittest.TestCase):
+
+    def stringRaises(self, string, exceptions):
+        with open(string) as f:
+            string = f.read()
+        with self.assertRaises(exceptions):
+            balancer = Balancer(string=string, print2d=True)
+
+    def test_illegal_entities(self):
+        self.stringRaises(
+            'blueprint_strings/illegal_entities.blueprint',
+            UnknownEntity)
+
+    def test_illegal_configurations(self):
+        self.stringRaises(
+            'blueprint_strings/illegal_belt_configuration.blueprint',
+            IllegalConfigurations)
+        self.stringRaises(
+            'blueprint_strings/illegal_splitter_configuration1.blueprint',
+            IllegalConfigurations)
+        self.stringRaises(
+            'blueprint_strings/illegal_splitter_configuration2.blueprint',
+            IllegalConfigurations)
+        self.stringRaises(
+            'blueprint_strings/illegal_underground_configuration1.blueprint',
+            IllegalConfigurations)
+        self.stringRaises(
+            'blueprint_strings/illegal_underground_configuration2.blueprint',
+            IllegalConfigurations)
+
+    def test_import(self):
+        with open('blueprint_strings/4x4_balancer.blueprint') as f:
+            string = f.read()
+        balancer = Balancer(string=string, print2d=True)
